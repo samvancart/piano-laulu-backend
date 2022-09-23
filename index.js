7f3027d2895ebde07fc9243fbbd4ab78b7c06f9d
samvancart <sam02cart@gmail.com>
2022-09-23 15:15:42 +0300

    send emails with nodemailer to ethereal

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config()
+
+const nodemailer = require('nodemailer');
+
 const cors = require('cors');
+const { json } = require('express');
 app.use(cors());
 
 app.use(express.json());
@@ -24,15 +29,45 @@ app.get('/api/customers', (req, res) => {
 });
 
 app.post('/api/send', (req, res) => {
-    const customer = req.body
-    console.log(customer)
-    res.json(customer)
+    const customer = req.body;
+    console.log(customer);
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: process.env.ETHEREAL_USER,
+            pass: process.env.ETHEREAL_PASSWORD
+        }
+    });
+
+    const mailOptions = {
+        from: 'info@keikat.fi',
+        to: 'keikat.testi@gmail.com',
+        subject: 'Keikka',
+        text: `Nimi: ${customer.name}
+            Sähköposti: ${customer.email}
+            Puhelin: ${customer.phone}
+            Viesti: ${customer.info}`
+    };
+    transporter.sendMail(mailOptions, (error, information) =>{
+        if(error){
+            console.log(error);
+            res.send('error');
+        } else {
+            // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(information));
+            // console.log('Email sent: ' + information.response)
+            res.send('success')
+        }
+    });
+
+    res.json(customer);
 
 });
 
 
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 });
